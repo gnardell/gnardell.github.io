5c593367e7cbf6ee34a397837cc0a1013c879ace
Giuseppe Nardella <gnardell1973@gmail.com>
2016-04-30 19:14:24 +0200

    Fixed main navigation

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -2,99 +2,99 @@
 //    IE Viewport
 //============================================================
 if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-    var msViewportStyle = document.createElement('style')
-    msViewportStyle.appendChild(
-        document.createTextNode(
-            '@-ms-viewport{width:auto!important}'
-        )
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
     )
-    document.querySelector('head').appendChild(msViewportStyle)
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
 }
 
 
 
 jQuery(document).ready(function ($) {
-//============================================================
-//    Main Navigation
-//============================================================
+  //============================================================
+  //    Main Navigation
+  //============================================================
 
-    $('.toggle-link').on('click', function(){
-        $('ul.main-nav').toggleClass('is-toggled-on');
-        $('ul.main-nav li').toggleClass('is-visible');
-        $('#toggle-link .icon-menu').toggleClass('rotate');
+  $('.toggle-link').on('click', function () {
+      $('.main-navigation').toggleClass('is-z-index');
+      $('ul.main-nav').toggleClass('is-toggled-on');
+      $('ul.main-nav li').toggleClass('is-visible');
+      $('#toggle-link .icon-menu').toggleClass('rotate');
     })
-//============================================================
-//    TABS, ACCORDION AND TOGGLES
-//============================================================
+    //============================================================
+    //    TABS, ACCORDION AND TOGGLES
+    //============================================================
 
-// TABS
+  // TABS
 
-    $('.tab-link').css('display', 'inline-block');
-    $('.tab-content').css('display', 'none');
-    $('.tabs').each(function (index) {
-        $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
-    });
-    $('.tabs').on('click', 'li > a.tab-link', function (event) {
-        if (!$(this).hasClass('is-active')) {
-            event.preventDefault();
-            var accordionTabs = $(this).closest('.tabs');
-            accordionTabs.find('.is-open').removeClass('is-open').hide();
+  $('.tab-link').css('display', 'inline-block');
+  $('.tab-content').css('display', 'none');
+  $('.tabs').each(function (index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+  $('.tabs').on('click', 'li > a.tab-link', function (event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.tabs');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
 
-            $(this).next().toggleClass('is-open').toggle();
-            accordionTabs.find('.is-active').removeClass('is-active');
-            $(this).addClass('is-active');
-        } else {
-            event.preventDefault();
-        }
-    });
-
-
-//ACCORDION
-
-    $('.submenu').css('display', 'none');
-    $('.js-accordion-trigger').on('click', function (e) {
-        var liItem = $(this).parent();
-        liItem.find('.submenu').slideToggle('slow');
-        // apply the toggle to the ul
-        liItem.toggleClass('is-expanded');
-        if (!liItem.hasClass('is-expanded')) {
-            $(this).blur();
-        }
-        e.preventDefault();
-    });
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
 
 
-//TOGGLES
+  //ACCORDION
 
-    $('.submenu').css('display', 'none');
-    $('.js-toggle-trigger').on('click', function (e) {
-        if (!$(this).parent().hasClass('is-expanded')) {
-            $('.toggle-link.is-expanded').toggleClass('is-expanded').children('.submenu').slideToggle('slow');
-        }
-        var liItem = $(this).parent();
-        liItem.find('.submenu').slideToggle('slow');
-        // apply the toggle to the ul
-        liItem.toggleClass('is-expanded');
-        if (!liItem.hasClass('is-expanded')) {
-            $(this).blur();
-        }
-        e.preventDefault();
-    });
+  $('.submenu').css('display', 'none');
+  $('.js-accordion-trigger').on('click', function (e) {
+    var liItem = $(this).parent();
+    liItem.find('.submenu').slideToggle('slow');
+    // apply the toggle to the ul
+    liItem.toggleClass('is-expanded');
+    if (!liItem.hasClass('is-expanded')) {
+      $(this).blur();
+    }
+    e.preventDefault();
+  });
 
 
-//============================================================
-//    FORMS ELEMENT
-//============================================================
+  //TOGGLES
 
-//INPUT FILE
+  $('.submenu').css('display', 'none');
+  $('.js-toggle-trigger').on('click', function (e) {
+    if (!$(this).parent().hasClass('is-expanded')) {
+      $('.toggle-link.is-expanded').toggleClass('is-expanded').children('.submenu').slideToggle('slow');
+    }
+    var liItem = $(this).parent();
+    liItem.find('.submenu').slideToggle('slow');
+    // apply the toggle to the ul
+    liItem.toggleClass('is-expanded');
+    if (!liItem.hasClass('is-expanded')) {
+      $(this).blur();
+    }
+    e.preventDefault();
+  });
 
-    $('.input-file').on('change', function () {
-        var fileDir = $('.input-file').val().split('\\');
-        var fileName = fileDir[fileDir.length - 1];
-        $('#input-file-name').text(fileName);
-    });
-    $('button[type="reset"]').click(function() {
-        $('#input-file-name').text('');
-    });
+
+  //============================================================
+  //    FORMS ELEMENT
+  //============================================================
+
+  //INPUT FILE
+
+  $('.input-file').on('change', function () {
+    var fileDir = $('.input-file').val().split('\\');
+    var fileName = fileDir[fileDir.length - 1];
+    $('#input-file-name').text(fileName);
+  });
+  $('button[type="reset"]').click(function () {
+    $('#input-file-name').text('');
+  });
 });
-
